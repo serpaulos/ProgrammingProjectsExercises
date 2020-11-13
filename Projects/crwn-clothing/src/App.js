9ce9feef95a81eae7ea41b49545cdd906b512d8f
Paulo Sergio <sergiopaulosilvajr@gmail.com>
2020-11-13 00:52:48 -0300

    update menu-item e navegacao

--- Projects/crwn-clothing/src/App.js
+++ Projects/crwn-clothing/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 
 //page components
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 
 
 function App() {
   return (
     <div>
-     <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>    
     </div>
   );
 }
