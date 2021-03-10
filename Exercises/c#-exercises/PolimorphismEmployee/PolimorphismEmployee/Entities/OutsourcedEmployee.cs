ba6cc4ad390720966d94d89d98ebdb3725be64f0
Paulo Sergio <sergiopaulosilvajr@gmail.com>
2021-03-10 19:11:35 -0300

    adicionado pasta de empregados usando polimorfismo

---
+++ Exercises/c#-exercises/PolimorphismEmployee/PolimorphismEmployee/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolimorphismEmployee.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AditionalCharge { get; set;}
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double aditionalCharge) : base(name, hours, valuePerHour)
+        {
+            AditionalCharge = aditionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AditionalCharge;
+        }
+    }
+}
